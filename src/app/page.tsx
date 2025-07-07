227922b3a4815396265843ee38ac1ae0a85ee7ec
rizkosefta <110314129+rizkosefta@users.noreply.github.com>
2025-07-07 23:18:38 +0700

    add header home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
+import Logo from "@/assets/images/logo.jpg";
 
 export default function Home() {
-  return <div className=""></div>
+  return <>
+  <header className="flex items-center justify-between px-4">
+        <span className="flex gap-x-2 items-center">
+          <span className="text-color1">
+          <img src={Logo.src} alt="Logo" className="h-8 w-auto" />
+          </span>
+          <span className="font-bold text-2xl">Katika Catering</span>
+        </span>
+      </header>
+  </>;
 }
